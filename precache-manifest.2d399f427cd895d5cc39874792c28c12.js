e19e9446ce26cad5f6178e149bd270fe613050d8
Frank Li <lifrank@grinnell.edu>
2020-07-17 15:17:15 -0500

    Updates

---
+++ precache-manifest.2d399f427cd895d5cc39874792c28c12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2225ca7d157260847d6434bf5b0ac5f9",
+    "url": "/terravis/index.html"
+  },
+  {
+    "revision": "c6d6423ca701d044baf6",
+    "url": "/terravis/static/js/2.be20c55b.chunk.js"
+  },
+  {
+    "revision": "7ed25655036eb5e36ee57c27d04702cc",
+    "url": "/terravis/static/js/2.be20c55b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "015b8378763db052a4c4",
+    "url": "/terravis/static/js/main.d63c8ede.chunk.js"
+  },
+  {
+    "revision": "c8a61d63ac2a090cc930",
+    "url": "/terravis/static/js/runtime-main.bb758952.js"
+  }
+]);
