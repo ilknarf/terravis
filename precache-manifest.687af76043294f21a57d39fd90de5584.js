9e33478dd5747fdec9e3f18f1b68a0ad380a274a
Frank Li <lifrank@grinnell.edu>
2020-08-10 11:03:45 -0500

    Updates

---
+++ precache-manifest.687af76043294f21a57d39fd90de5584.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba5da1a1466f165bd30aab24012f0ea3",
+    "url": "/terravis/index.html"
+  },
+  {
+    "revision": "51ff02a571f031b0babd",
+    "url": "/terravis/static/js/2.f0a6b374.chunk.js"
+  },
+  {
+    "revision": "5dd2ea6eb573f5e29a67cdf8baf345dd",
+    "url": "/terravis/static/js/2.f0a6b374.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11128c2efab6385273b2",
+    "url": "/terravis/static/js/main.5229f961.chunk.js"
+  },
+  {
+    "revision": "c8a61d63ac2a090cc930",
+    "url": "/terravis/static/js/runtime-main.bb758952.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/terravis/precache-manifest.2c91fda52af820b8b10cea06c5732c62.js"
+  "/terravis/precache-manifest.687af76043294f21a57d39fd90de5584.js"
 );
 
 self.addEventListener('message', (event) => {
